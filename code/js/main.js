ec3f5baf7c60af2a699917e46e5303b40ac1efca
nik@mihalick <nik@mihalick.name>
2012-10-17 21:31:00 -0400

    added the prompt to the new page.

--- code/js/main.js
+++ code/js/main.js
@@ -3,6 +3,13 @@
  */
 var app = {};
 
+app.templates = {};
+app.templates.start_new_design = '<div class="hero-unit">';
+app.templates.start_new_design += '<h1>hi!</h1>';
+app.templates.start_new_design += '<p>would you like to build a responsive design?</p>';
+app.templates.start_new_design += '<a class="btn btn-primary btn-large">YES</a>';
+app.templates.start_new_design += '</div>';
 
 $(document).ready(function() {
+    $('#content').html(app.templates.start_new_design);
 });
